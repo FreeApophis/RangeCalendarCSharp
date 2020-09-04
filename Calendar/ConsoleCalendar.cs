b9880320da93aebb0969ce2dff2403c4e350e088
Thomas Bruderer <apophis@apophis.ch>
2020-09-04 15:47:44 +0200

    some simple Refactoring * extract 2 modules * final functional refactorings * accept 2 parameters from console

---
+++ Calendar/ConsoleCalendar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Funcky;
+using MoreLinq;
+
+namespace Calendar
+{
+    internal class ConsoleCalendar
+    {
+        private const int HorizontalMonths = 3;
+
+        internal static string FromYear(int year)
+            => DaysInYear(year)
+                .GroupAdjacent(ByMonth)
+                .Select(MonthLayouter.DefaultLayout)
+                .Batch(HorizontalMonths)
+                .Select(m => m.Transpose())
+                .Select(JoinLine)
+                .SelectMany(Functional.Identity)
+                .JoinString(Environment.NewLine);
+
+        private static IEnumerable<DateTime> DaysInYear(int year)
+            => MoreEnumerable
+                .Generate(new DateTime(year, 1, 1), day => day + new TimeSpan(1, 0, 0, 0))
+                .TakeWhile(day => day < new DateTime(year + 1, 1, 1));
+
+        private static int ByMonth(DateTime date)
+            => date.Month;
+
+        private static IEnumerable<string> JoinLine(IEnumerable<IEnumerable<string>> lines)
+            => lines.Select(line => string.Join(' ', line));
+    }
+}
--- Calendar/Extensions.cs
+++ Calendar/Extensions.cs
@@ -4,12 +4,15 @@ namespace Calendar
 {
     static class Extensions
     {
-        public static string Center(this string text, int width)
-            => new string(' ', (width - text.Length) / 2 + 1)
-               + text
-               + new string(' ', (width - text.Length) / 2);
-
         public static string JoinString(this IEnumerable<string> source, string delimiter)
             => string.Join(delimiter, source);
+
+        public static string Center(this string text, int width)
+            => (width - text.Length) switch
+            {
+                0 => text,
+                1 => $" {text}",
+                _ => Center($" {text} ", width),
+            };
     }
 }
