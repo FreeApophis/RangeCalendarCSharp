9e293c267326bcd4aadff2f1b8f23a07f16cd704
Thomas Bruderer <apophis@apophis.ch>
2020-09-07 08:06:48 +0200

    With the argument "stream" we show the possibility of a lazy stream.

--- Calendar/ConsoleCalendar.cs
+++ Calendar/ConsoleCalendar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Funcky;
+using Funcky.Monads;
 using MoreLinq;
 
 namespace Calendar
@@ -10,20 +11,26 @@ namespace Calendar
     {
         private const int HorizontalMonths = 3;
 
-        internal static string FromYear(int year)
-            => DaysInYear(year)
+        internal static string SingleYear(int year)
+            => ArrangeCalendarPage(year, year => GetDays(year, Option.Some(year)))
+                .JoinString(Environment.NewLine);
+
+        internal static IEnumerable<string> Stream(int year)
+            => ArrangeCalendarPage(year, year => GetDays(year));
+
+        private static IEnumerable<string> ArrangeCalendarPage(int year, Func<int, IEnumerable<DateTime>> dayEnumerator)
+            => dayEnumerator(year)
                 .GroupAdjacent(ByMonth)
                 .Select(MonthLayouter.DefaultLayout)
                 .Batch(HorizontalMonths)
                 .Select(m => m.Transpose())
                 .Select(JoinLine)
-                .SelectMany(Functional.Identity)
-                .JoinString(Environment.NewLine);
+                .SelectMany(Functional.Identity);
 
-        private static IEnumerable<DateTime> DaysInYear(int year)
+        private static IEnumerable<DateTime> GetDays(int fromYear, Option<int> toYear = default)
             => MoreEnumerable
-                .Generate(new DateTime(year, 1, 1), day => day + new TimeSpan(1, 0, 0, 0))
-                .TakeWhile(day => day < new DateTime(year + 1, 1, 1));
+                .Generate(new DateTime(fromYear, 1, 1), day => day + new TimeSpan(1, 0, 0, 0))
+                .TakeWhile(day => toYear.Match(true, year => day < new DateTime(year + 1, 1, 1)));
 
         private static int ByMonth(DateTime date)
             => date.Month;
