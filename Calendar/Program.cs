58127ab03453ce55e5de4af13cbe8a235ff40a9f
Thomas Bruderer <apophis@apophis.ch>
2020-09-07 08:33:03 +0200

    extract console arguments extract SelectArguments

--- Calendar/Program.cs
+++ Calendar/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using Funcky.Extensions;
 using System.Globalization;
-using System.Linq;
-using Funcky.Monads;
 using Funcky;
+using Funcky.Extensions;
+using static Calendar.ConsoleArguments;
 
 namespace Calendar
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
@@ -23,32 +22,5 @@ namespace Calendar
 
         private static Action PrintSingleYear(string[] args)
             => () => Console.WriteLine(ConsoleCalendar.SingleYear(GetCalendarYear(args)));
-
-        private static Option<Unit> GetStreamingMode(string[] args)
-            => args
-            .WhereSelect(IsStreamingArgument)
-            .FirstOrNone();
-
-        private static Option<Unit> IsStreamingArgument(string arg)
-            => arg == "stream"
-                ? Option.Some(Unit.Value)
-                : Option<Unit>.None();
-
-        private static int GetCalendarYear(string[] args) =>
-            args
-                .WhereSelect(cli => cli.TryParseInt())
-                .FirstOrNone()
-                .GetOrElse(DateTime.Now.Year);
-
-        private static Option<CultureInfo> GetCultureInfo(string[] args)
-            => args
-            .WhereSelect(ToCultureInfo)
-            .FirstOrNone();
-
-        private static Option<CultureInfo> ToCultureInfo(string cultureString)
-            => CultureInfo
-                .GetCultures(CultureTypes.AllCultures & ~CultureTypes.NeutralCultures)
-                .Where(culture => culture.Name == cultureString)
-                .FirstOrNone();
     }
 }
